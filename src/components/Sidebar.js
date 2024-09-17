31cc09530f2d73aa7686dc8bdaadbf3e0845a345
Priscilla-22 <priscillamw123@gmail.com>
2024-09-17 12:05:20 +0300

    refactor: styles and feat's

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -6,11 +6,11 @@ function Sidebar() {
       {/* Logo and Shamba Records */}
       <div className='flex items-center mb-4'>
         <img
-          src='../../public/shamba-records-logo.jpeg'
+          src='/shamba-records-logo1.png'
           alt='Shamba Records Logo'
           className='h-8 w-8 mr-2'
         />
-        <h1 className='text-xl font-bold'>Shamba Records</h1>
+        <h1 className='text-xl font-bold text-white'>Shamba Records</h1>
       </div>
 
       <hr className='border-gray-300 mb-4 border-t-1' />
@@ -18,13 +18,15 @@ function Sidebar() {
       {/* Profile Section */}
       <div className='flex items-center mb-6'>
         <img
-          src='../../public/profile-pic.jpg'
+          src='/profile-pic2.avif'
           alt='Profile'
-          className='h-12 w-12 rounded-full mr-3'
+          className='h-14 w-14 rounded-full mr-3 border-gray-300 border-4 object-cover object-center'
         />
         <div>
-          <p className='text-lg font-semibold'>Priscilla Wakahia</p>
-          <p className='text-sm text-gray-600'>Logged In</p>
+          <p className='text-base font-medium text-custom-smokeWhite hover:text-white'>
+            Priscilla Wakahia
+          </p>
+          {/* <p className='text-sm text-gray-600'>Logged In</p> */}
         </div>
       </div>
 
