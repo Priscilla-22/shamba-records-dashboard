7d764636ddf63d8d9fa7ca63f490c4165e3b9b30
Priscilla-22 <priscillamw123@gmail.com>
2024-09-17 12:03:38 +0300

    feat: import css styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
-import { TfiMenuAlt } from 'react-icons/tfi';  // Import the hamburger menu icon
-import { CiMenuKebab } from 'react-icons/ci';  // Import the three dots icon
+import { TfiMenuAlt } from 'react-icons/tfi'; // Import the hamburger menu icon
+import { CiMenuKebab } from 'react-icons/ci'; // Import the three dots icon
 import Sidebar from './components/Sidebar';
 import Dashboard from './components/Dashboard';
-import "./index.css";
+import './index.css';
 
 function App() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
@@ -14,27 +14,27 @@ function App() {
   };
 
   return (
-    <div className='flex h-screen bg-custom-smokeWhite relative'>
-      {/* Toggle Button */}
-      <div className='absolute top-4 left-4'>
-        <button
-          onClick={toggleSidebar}
-          className='bg-gray-800 text-white p-2 rounded-full focus:outline-none'
-        >
-          {isSidebarOpen ? (
-            <TfiMenuAlt size={24} />
-          ) : (
-            <CiMenuKebab size={24} />
-          )}
-        </button>
-      </div>
-
+    <div className='flex h-screen bg-custom-smokeWhite'>
       {/* Sidebar */}
       {isSidebarOpen && <Sidebar />}
 
       {/* Main Content */}
-      <div className={`flex-grow ${isSidebarOpen ? 'ml-64' : ''}`}>
+      <div className={`flex-grow ${isSidebarOpen ? 'ml-1' : ''}`}>
         <Dashboard />
+      </div>
+
+      {/* Toggle Button */}
+      <div
+        className={`absolute top-4 ml-2 ${
+          isSidebarOpen ? 'left-64' : 'left-4'
+        } transition-all duration-300`}
+      >
+        <button
+          onClick={toggleSidebar}
+          className='bg-gray-800 text-white p-2 rounded-full focus:outline-none'
+        >
+          {isSidebarOpen ? <TfiMenuAlt size={24} /> : <CiMenuKebab size={24} />}
+        </button>
       </div>
     </div>
   );
