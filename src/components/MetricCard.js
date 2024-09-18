52db273a3de8a0e97abb9725231a76da9cdda123
Priscilla-22 <priscillamw123@gmail.com>
2024-09-18 16:05:07 +0300

    style: refactor icon height

--- src/components/MetricCard.js
+++ src/components/MetricCard.js
@@ -18,7 +18,7 @@ function MetricCard({ metric, gradient, iconSrc, maxValue = 10000 }) {
           zIndex: 10,
         }}
       >
-        <img src={iconSrc} alt='Icon' className='w-8 h-5 object-cover' />
+        <img src={iconSrc} alt='Icon' className='w-8 h-6 object-cover' />
       </div>
 
       <h2 className='text-base font-bold mt-4'>{metric.label}</h2>
